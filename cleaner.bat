81412856cba2787548db83e54b2d18ac47631394
Andrew <38564365+fastb1t@users.noreply.github.com>
2019-07-01 10:54:27 +0300

    Updated project files

--- cleaner.bat
+++ cleaner.bat
@@ -1,14 +1,7 @@
 @echo off
 
-del /S /Q J2ME\Tic-Tac-Toe\bin
-del /S /Q J2ME\Tic-Tac-Toe\classes
-
-:: del /S /Q J2ME\Tic-Tac-Toe\history
-:: del /S /Q J2ME\Tic-Tac-Toe\libs
-:: del /S /Q J2ME\Tic-Tac-Toe\scripts
-
-del /S /Q J2ME\Tic-Tac-Toe\src\build
-
-del /S /Q Tic-Tac-Toe/bin
+del /S /Q Tic-Tac-Toe\bin
+del /S /Q Tic-Tac-Toe\classes
+del /S /Q Tic-Tac-Toe\src\build
 
 exit
