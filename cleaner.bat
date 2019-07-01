2024b342ca234928980076b6bd3cdda22f9b829d
Andrew <38564365+fastb1t@users.noreply.github.com>
2019-07-01 11:10:14 +0300

    Update cleaner.bat

--- cleaner.bat
+++ cleaner.bat
@@ -1,7 +1,7 @@
 @echo off
 
-del /S /Q Tic-Tac-Toe\bin
-del /S /Q Tic-Tac-Toe\classes
+del /S /Q Tic-Tac-Toe\bin\Tic-Tac-Toe.jad
+del /S /Q Tic-Tac-Toe\bin\Tic-Tac-Toe.jar
 del /S /Q Tic-Tac-Toe\src\build
 
 exit
